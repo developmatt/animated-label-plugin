7144b61381d6020f218e4d253a79dbe8ebdbea66
Matheus Godoi <matheusasdev@gmail.com>
2018-09-15 11:38:14 -0300

    Inserting jquery plugin structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-console.log("webpack sass js");
+jQuery = $ = require('jquery');
+
+(function( $ ) {
+    $.fn.myPlugin = function() {
+        
+        let form = this;
+        let label = $(form).find('label');
+        let input = $(form).find('input');
+
+        $(input).focusin(function(){
+            $(label).css('bottom', '100%');
+        })
+        
+        $(input).focusout(function(){
+            if($(label).val() == ''){
+                $(label).css('bottom', '0');
+            }
+        })
+    };
+})( jQuery );
