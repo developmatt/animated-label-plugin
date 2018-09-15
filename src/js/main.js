1deac7c61cae8018dc7e876673e159a15a55f3f5
Matheus Godoi <matheusasdev@gmail.com>
2018-09-15 15:11:36 -0300

    Adapting plugin to multiple fields

--- src/js/main.js
+++ src/js/main.js
@@ -2,23 +2,24 @@ import * as componentsStyle from './componentsStyle';
 
 (function( $ ) {
     $.fn.animatedLabelPlugin = function() {
-        
-        let form = this;        
-        let label = $(form).find('label');
-        let input = $(form).find('input');
-        
-        $(form).css(componentsStyle.formStyle);
-        $(label).css(componentsStyle.labelStyle);
-        $(input).css(componentsStyle.inputStyle);
+        $(this).each(function(){
+            let form = this;
+            let label = $(form).find('label');
+            let input = $(form).find('input');
+            
+            $(form).css(componentsStyle.formStyle);
+            $(label).css(componentsStyle.labelStyle);
+            $(input).css(componentsStyle.inputStyle);
 
-        $(input).focusin(function(){
-            $(label).css('bottom', '100%');
-        })
-        
-        $(input).focusout(function(){
-            if($(label).val() == ''){
-                $(label).css('bottom', '0');
-            }
+            $(input).focusin(function(){
+                $(label).css('bottom', '100%');
+            })
+            
+            $(input).focusout(function(){
+                if($(label).val() == ''){
+                    $(label).css('bottom', '0');
+                }
+            })
         })
     };
 })( $ );
